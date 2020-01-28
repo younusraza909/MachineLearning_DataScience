fd5e6799648bc8ed2bdad3aec693f5130b0777ff
younusraza909 <younusraza909@gmail.com>
2020-01-28 10:46:59 +0500

    Random Forest Regression

--- Regression/Random Forest Regression/Random Forest Regressor.py
+++ Regression/Random Forest Regression/Random Forest Regressor.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 28 09:47:44 2020
+Created on Tue Jan 28 10:35:22 2020
 
 @author: Raza
 """
-
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 #Loading Data Set
-data=pd.read_csv("DTR.csv")
+data=pd.read_csv("RFR.csv")
 
 #Splitting Data 
 X=data.iloc[:,0:1].values
 Y=data.iloc[:,1].values
 
 #Regression
-from sklearn.tree import DecisionTreeRegressor
-DTR_reg=DecisionTreeRegressor(random_state=0)
-DTR_reg.fit(X,Y)
+from sklearn.ensemble import RandomForestRegressor
+RFR_reg=RandomForestRegressor(random_state=0,n_estimators=10)
+RFR_reg.fit(X,Y)
 
 #Visualization
 plt.scatter(X,Y,color="red")
-plt.plot(X,DTR_reg.predict(X),color="blue")
-plt.title(" DTR MODEL Position Vs Salary")
+plt.plot(X,RFR_reg.predict(X),color="blue")
+plt.title(" RFR MODEL Position Vs Salary")
 plt.xlabel("Positon Of Employee")
 plt.ylabel("Salary")
 plt.show()
@@ -33,8 +32,8 @@ plt.show()
 x_grid=np.arange(min(X),max(X),0.1)
 x_grid=x_grid.reshape(len(x_grid),1)
 plt.scatter(X,Y,color="red")
-plt.plot(x_grid,DTR_reg.predict(x_grid),color="blue")
-plt.title(" DTR MODEL Position Vs Salary")
+plt.plot(x_grid,RFR_reg.predict(x_grid),color="blue")
+plt.title(" RFR MODEL Position Vs Salary")
 plt.xlabel("Positon Of Employee")
 plt.ylabel("Salary")
 plt.show()
@@ -42,5 +41,5 @@ plt.show()
 
 
 #Prediction
-DTR_reg.predict([[6.5]])
+RFR_reg.predict([[6.5]])
 
